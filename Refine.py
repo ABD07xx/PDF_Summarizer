e528a9a214a20a02be32866b2b51445a5c88036d
ABD07xx <abdullahfazili3614@gmail.com>
2024-05-13 19:57:12 +0530

    Restructured Code

--- Refine.py
+++ Refine.py
@@ -1,5 +1,4 @@
 from langchain.chains.summarize import load_summarize_chain
-from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from PyPDF2 import PdfReader
@@ -7,18 +6,15 @@ from config import llm_config
 
 PDF_PATH = "./PDF/Bartleby, The Scrivener A Story Of Wall-street Author Herman Melville.pdf"
 
-
 # Function to extract text from a PDF file
 def extract_text_from_pdf(pdf_path):
     pdfreader = PdfReader(pdf_path)
     return "".join([page.extract_text() for page in pdfreader.pages if page.extract_text()])
 
-
 # Function to split text into chunks using RecursiveCharacterTextSplitter
-def  split_text(text, chunk_size=25000):
+def split_text(text, chunk_size=25000):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size)
     return text_splitter.create_documents([text])
-
 
 # Function to initialize the ChatGPT model
 def initialize_llm(config):
@@ -29,23 +25,18 @@ def run_summarization_chain(llm, chunks):
     summarization_chain = load_summarize_chain(
         llm,
         chain_type='refine',
-        verbose=True)
-
+        verbose=True
+    )
     return summarization_chain.run(chunks)
+
 # Main function to orchestrate the summarization process
 def main():
     text = extract_text_from_pdf(PDF_PATH)
-
     llm = initialize_llm(llm_config)
-
     chunks = split_text(text)
-
-    refined_summary = run_summarization_chain(
-        llm, chunks
-    )
- 
-    print("\n\n\n\n\n\n")
-    print("Refined Answer: \n", refined_summary)
+    refined_summary = run_summarization_chain(llm, chunks)
+    
+    print("Refined Summary: \n", refined_summary)
 
 if __name__ == "__main__":
     main()
--- Sequential.py
+++ Sequential.py
@@ -5,25 +5,22 @@ from config import llm_config
 
 PDF_PATH = "./PDF/Bartleby, The Scrivener A Story Of Wall-street Author Herman Melville.pdf"
 
-
 # Function to extract text from a PDF file
 def extract_text_from_pdf(pdf_path):
     pdfreader = PdfReader(pdf_path)
     return "".join([page.extract_text() for page in pdfreader.pages if page.extract_text()])
 
-
 # Function to split text into chunks using RecursiveCharacterTextSplitter
-def  split_text(text, chunk_size=25000):
+def split_text(text, chunk_size=25000):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size)
     return text_splitter.create_documents([text])
-
 
 # Function to initialize the ChatGPT model
 def initialize_llm(config):
     return ChatOpenAI(**config)
 
-
-def run_custom_sequential_model(llm,chunks):
+# Function to run the custom sequential model for summarization
+def run_custom_sequential_model(llm, chunks):
     final_context = ""
     for i in range(len(chunks)):
         local_context = ""
@@ -33,8 +30,7 @@ def run_custom_sequential_model(llm,chunks):
         encapsulate both the previous summary and the summary of the new chunk provided
         """
         summary = llm.invoke(prompt1)
-        local_context += (list(summary)[0][1])
-        #print(f"For loop {i} I have generated a new prompt")
+        local_context += list(summary)[0][1]
     final_context = local_context
     prompt2 = f"""You will receive a document.Your task is to provide a concise summary of the text, focusing on the key points, important facts, or significant findings.
     Ensure that the summary is clear, coherent, and flows logically. Organize the main points in a structured manner, aiming to capture the essence of the document
@@ -48,16 +44,10 @@ def run_custom_sequential_model(llm,chunks):
 # Main function to orchestrate the summarization process
 def main():
     text = extract_text_from_pdf(PDF_PATH)
-
     llm = initialize_llm(llm_config)
-
     chunks = split_text(text)
-
-    sequential_sumamry = run_custom_sequential_model(
-        llm, chunks
-    )
-
-    print("Sequential Summary:\n", list(sequential_sumamry)[0][1])
+    sequential_summary = run_custom_sequential_model(llm, chunks)
+    print("Sequential Summary:\n", list(sequential_summary)[0][1])
 
 if __name__ == "__main__":
     main()
